2e387e285fe48508ef1eab4dc861fd1f297b58f5
injifannoo <injifanotu@gmail.com>
2024-11-19 17:13:07 +0300

    some changes

---
+++ src/components/welcome.jsx
@@ -0,0 +1,14 @@
+// src/components/Welcome.jsx
+import React from 'react';
+
+const Welcome = () => {
+    return (
+        <div className="flex items-center justify-center h-screen bg-gray-100">
+            <h1 className="text-4xl font-bold text-blue-600">
+                WELCOME To Raba Technologies
+            </h1>
+        </div>
+    );
+};
+
+export default Welcome;
