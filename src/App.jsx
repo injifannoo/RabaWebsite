6f0633674a59453d7afb964ee2759a55f236a4c4
Naol Kecha <naolkecha@gmail.com>
2024-11-24 22:11:09 +0300

    services

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import Welcome from './components/welcome'
 import './App.css'
+import Services from './components/Services';
 
 const App = () => {
   return (
       <div>
           <Welcome />
+          <Services/>
       </div>
   );
 };
