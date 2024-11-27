0e1361f36d911008ed8506ae5b02e2f5729adb77
injifannoo <injifanotu@gmail.com>
2024-11-27 21:53:29 +0300

    Blogs card is added

---
+++ src/components/BlogCard.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const BlogCard = ({ blog }) => {
+  return (
+    <div className="bg-white p-4 rounded-lg shadow-md hover:shadow-lg">
+      <h3 className="text-xl font-semibold">{blog.title}</h3>
+      <p className="text-gray-700">{blog.content}...</p> {/*slice(0, 100) */}
+      <p className="text-gray-700">{blog.conclusion}...</p>
+      <p className="text-sm text-gray-500">By {blog.tags}</p>
+      <p className="text-sm text-gray-500">By {blog.author}</p>
+      <p className="text-sm text-gray-500">{new Date(blog.date).toLocaleDateString()}</p>
+    </div>
+  );
+};
+
+export default BlogCard;
