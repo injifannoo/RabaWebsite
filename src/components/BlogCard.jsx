828ebad3cc3ace39e82c5744f37094203764238f
injifannoo <injifanotu@gmail.com>
2024-12-04 22:56:03 +0300

    blogs is updated

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const BlogCard = ({ blog }) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const handleToggle = () => {
+    setIsExpanded(!isExpanded);
+  };
+
   return (
     <div className="bg-white p-4 rounded-lg shadow-md hover:shadow-lg">
-      <h3 className="text-xl font-semibold">{blog.title}</h3>
-      <p className="text-gray-700">{blog.content}...</p> {/*slice(0, 100) */}
-      <p className="text-gray-700">{blog.conclusion}...</p>
-      <p className="text-sm text-gray-500">By {blog.tags}</p>
-      <p className="text-sm text-gray-500">By {blog.author}</p>
-      <p className="text-sm text-gray-500">{new Date(blog.date).toLocaleDateString()}</p>
+      <div className="text-justify ml-10 my-10">
+     <div className="grid grid-cols-2 gap-6 px-4 md-4"> 
+      <p className="text-sm text-gray-500 align-baseline">{new Date(blog.date).toLocaleDateString()}</p>
+      <p className="text-sm text-gray-500 align-middle">By {blog.author}</p>
+      </div>
+      <h3 className="text-xl font-semibold mb-4">{blog.title}</h3>
+      <p className="text-gray-700 text-justify">
+        {isExpanded ? blog.content : `${blog.content.slice(0, 100)}...`}
+      </p>
+      {/* <p className="text-gray-700">{blog.conclusion}</p> */}
+      <p className="text-sm text-gray-500">Tags: {blog.tags}</p>
+      <button
+        onClick={handleToggle}
+        className="mt-2 text-blue-500 hover:underline focus:outline-none"
+      >
+        {isExpanded ? 'Show Less' : 'Read More'}
+      </button>
+      </div>
     </div>
   );
 };
--- src/pages/BlogPage.jsx
+++ src/pages/BlogPage.jsx
@@ -19,7 +19,7 @@ const LatestBlogs = () => {
   return (
     <div>
       <h2 className="text-3xl font-bold text-center mb-8">Latest Blogs</h2>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-1 gap-6 px-4">
+      <div className="grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-2 gap-6 px-4  mx-10">
         {blogs.slice(0, 3).map((blog) => (
           <BlogCard key={blog._id} blog={blog} />
         ))}
