62d7585ab4c138b67bd1fe3513e382cfae02d8d6
injifannoo <injifanotu@gmail.com>
2024-12-05 14:29:00 +0300

    Logo is updated

--- src/components/logo.jsx
+++ src/components/logo.jsx
@@ -5,8 +5,12 @@ const Logo = () => {
   return (
     <a href="/" className="flex items-center">
       <img src={LogoImage} alt="Raba Technologies Logo" className="h-10 w-auto" />
-      
+      <div className="flex flex-col">
       <span className="ml-2 text-lg font-semibold text-white">Raba Technologies</span>
+      <span className="text-sm text-white italic -mt-2">
+        Warriors for change, champions for success!
+      </span>
+      </div>
     </a>
   );
 };
