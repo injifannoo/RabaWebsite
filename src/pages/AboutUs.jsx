6e5c183c3939de7f370158cc512a973f435a5f7a
injifannoo <injifanotu@gmail.com>
2024-12-05 14:29:24 +0300

    About section is updated

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,28 +1,44 @@
-import aboutUs from '../assets/images/about-us.png';  // Import image
+import aboutUs from "../assets/images/about-us.png";
+import { useNavigate } from "react-router-dom";
 
-const AboutUs = () => (
+const AboutUs = () => {
+    const navigate = useNavigate();
+    return (
+        <div className="flex flex-row md:flex-row items-center md:justify-between -mt-8">
+            <div className="flex flex-col w-full md:w-1/2 text-left py-0 px-8">
+                <h1 className="text-3xl font-bold text-center">About Us</h1>
+                <p className="mt-4 text-lg text-start text-gray-700">
+                    Raba Technologies is a software development startup with a mission to build robust platforms that solve our customers' and society's problems. 
+                    We aim to deliver innovative software solutions to businesses of all sizes.
+                </p>
+                <div className="flex justify-evenly items-center mt-4">
+                    <button
+                        className="bg-blue-600 py-1 px-2 text-xl text-white rounded-lg"
+                        onClick={() => navigate("/learn-more")}
+                    >
+                        LEARN MORE
+                    </button>
+                    <button
+                            onClick={() => {    
+                              console.log("Navigating to Learn More");
+                              navigate("/teams")}}
+                        id="teams"
+                        className="border border-black py-1 px-2 text-xl text-blue-600 rounded-lg"
+                    >
+                        OUR TEAM
+                    </button>
+                </div>
+            </div>
 
-    <div className="flex flex-row md:flex-row items-center md:justify-between -mt-8">  {/* flex flex-col md:flex-row items-center md:justify-between */}
-        <div className=" flex flex-col w-full md:w-1/2 text-left py-0 px-8"> {/*w-full md:w-1/2 text-left*/}
-        <h1 className="text-3xl font-bold text-center">About Us</h1>
-        <p className="mt-4 text-lg text-start text-gray-700">
-      Raba technologies is a software development startup with a mission to build robust platforms that solve our customers' and society's problems. 
-      We aim to deliver innovative software solutions to businesses of all sizes.
-      </p>
-      <div className='flex flex-row text-center'>
-        <button className='bg-blue-600 text-1xl text-center py-1 px-2 text-xl text-white'> LEARN MORE</button>
-        <button className='border border-black  text-center py-1 px-2 text-xl text-blue-600'>CONTACT US</button>
-      </div>
-    </div>
-    <div className="w-full md:w-1/2 mt-0 md:mt-0 py-0 px-8">{/*w-full md:w-1/2 mt-4 md:mt-0*/}
-        <img 
-      src={aboutUs}
-      alt="About Us" 
-      class="max-w-full h-auto object-cover"
-    />
-    </div>
-    </div>
-  );
-  
-  export default AboutUs;
-  
+            <div className="w-full md:w-1/2 mt-0 md:mt-0 py-0 px-8">
+                <img 
+                    src={aboutUs}
+                    alt="About Us"
+                    className="max-w-full h-auto object-cover"
+                />
+            </div>
+        </div>
+    );
+};
+
+export default AboutUs;
