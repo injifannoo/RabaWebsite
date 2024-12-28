33a3714c9c5cd5793d443b58782032bd98833735
injifannoo <injifanotu@gmail.com>
2024-12-28 22:59:27 +0300

    changes to Blog

--- client/src/pages/BlogPage.jsx
+++ client/src/pages/BlogPage.jsx
@@ -1,37 +1,3 @@
-// import React, { useState, useEffect } from 'react';
-// import axios from 'axios';
-// import BlogCard from '../components/BlogCard';
-
-// const LatestBlogs = () => {
-//   const [blogs, setBlogs] = useState([]);
-
-//   useEffect(() => {
-//     // Fetch blogs from the backend API
-//     axios.get('http://localhost:3000/api/blogs')
-//       .then((response) => {
-//         setBlogs(response.data);
-//       })
-//       .catch((error) => {
-//         console.error('Error fetching blogs:', error);
-//       });
-//   }, []);
-
-//   return (
-//     <div className="bg-white w-full ">
-//       <div className="flex flex-col justify-center items-center my-10">
-//         <h2 className="text-center text-3xl font-bold text-black md:mb-12"> Recent Blogs</h2>
-//         <div className="line"></div>
-//       </div>
-//       <div className="grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-2 gap-6 md:px-4 md:mx-10">
-//         {blogs.slice(0,3).map((blog) => (
-//           <BlogCard key={blog._id} blog={blog} />
-//         ))}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default LatestBlogs;
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import BlogCard from '../components/BlogCard';
@@ -54,7 +20,9 @@ const LatestBlogs = () => {
   return (
     <div className="bg-white w-full">
       <div className="flex flex-col justify-center items-center my-10">
-        <h2 className="text-center text-3xl font-bold text-black md:mb-12">Recent Blogs</h2>
+        <h2 className="text-center text-3xl font-bold  text-blue-700 mb-3">Recent Blogs</h2>
+        <hr className="w-24 h-1 bg-blue-500 border-0 rounded " />
+
         <div className="line"></div>
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-2 gap-6 md:px-4 md:mx-10">
